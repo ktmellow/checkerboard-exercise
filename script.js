b900862bb9c4bc39fa9666602658e6bd6b050006
ktmellow <letsspamkt@gmail.com>
2016-02-29 18:59:26 -0800

    checkers again

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Your JS goes here
+var tiles = 63;
+for (var i=0; i < tiles; i++){
+  document.body.appendChild(document.createElement('div'));
+}
+var allTiles = document.querySelectorAll('div');
+for (var i=0; i < allTiles.length; i++) {
+  if (i % 2 === 0) {
+    allTiles[i].style.backgroundColor = "black";
+  } else {
+    allTiles[i].style.backgroundColor = "red";
+  }
+  allTiles[i].style.backgroundColor = getRandomColor();
+  allTiles[i].style.width = "11.1%";
+  allTiles[i].style.paddingBottom = "11.1%";
+  allTiles[i].style.cssFloat = "left";
+}
